6e682d04864be0319b50ce7b72c3ef132d06bdb8
suraj <surajj3404@gmail.com>
2024-01-28 08:14:37 +0530

    update

--- contracts/src/ReTEX.sol
+++ contracts/src/ReTEX.sol
@@ -20,7 +20,6 @@ contract ReTEX {
     mapping(string => address) public nameToAddress;
 
     mapping(address => UserData) public UserDatamap;
-    mapping(address => address[]) public userFavorites;
     event TradeInitiated(address indexed user, address indexed selectedUser, uint256 tradeAmount);
 
     function setValues(uint256 _value1, uint256 _value2) external {
@@ -62,26 +61,10 @@ contract ReTEX {
         UserData storage user = UserArray[index];
         return (user.produced, user.consumed, user.name);
     }
-
-    function addToFavorites(string memory _name) external {
-        userFavorites[msg.sender].push(nameToAddress[_name]);
-    }
-
-   function getFavorites() external view returns (address[] memory) {
-        return userFavorites[msg.sender];
-    }
-
+    
     function setTrade(string memory _name, uint256 tradeResource) external  {
         require(tradeResource > 0, "Trade amount must be greater than zero");
         require(tradeResource > UserDatamap[msg.sender].produced - UserDatamap[msg.sender].consumed, "Trade amount must be more than available");
-        bool isFavorite = false;
-        for (uint256 i = 0; i < userFavorites[msg.sender].length; i++) {
-            if (userFavorites[msg.sender][i] == nameToAddress[_name]) {
-                isFavorite = true;
-                break;
-            }
-        }
-        require(isFavorite, "Selected user is not in favorites");
         UserDatamap[msg.sender].produced -= tradeResource;
         UserDatamap[nameToAddress[_name]].consumed += tradeResource;
         emit TradeInitiated(msg.sender, nameToAddress[_name], tradeResource);
